26f909b2faa4c12429e9e9798938bc1fcd43ad05
SabryCat <sacdawcat@gmail.com>
2022-02-02 16:54:54 +0100

    add salida por pantalla

--- 01_PrimerProyectoGit/src/git/main/Main.java
+++ 01_PrimerProyectoGit/src/git/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos a nuestra versiones con Git");
-
+		System.out.println("Esto Es Otro Cambio en el Proyecto");
 
 	}
 
